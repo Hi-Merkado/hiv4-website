9a3e0a4506eb4564d68fdc943411a738b896a56f
robeygo <34327876+robeygo@users.noreply.github.com>
2023-03-13 11:37:26 +0800

    Update Featured.vue

--- components/Featured.vue
+++ components/Featured.vue
@@ -58,7 +58,7 @@
                 </div>
                 <div>
                     <a :href="defaultLink" class="uppercase text-xs flex items-center gap-2 leading-6 mt-1">
-                        <span class="inline leading-6">Browse All <span class="font-bold mx-1"></span> Listings</span>
+                        <span class="inline leading-6">Browse All <span class="font-bold mx-1">{{ ListingsStore.listings.data.meta.total.toLocaleString() }}</span> Listings</span>
                         <font-awesome-icon icon="arrow-right" class="text-gray-400" :style="{ color: '#a1a1aa', height: '14px', margin: '10px 0' }"/>
                     </a>
                 </div>
