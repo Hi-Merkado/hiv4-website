28d29e5657ea2a0ca5e5ed21a9d47215bc4c0a89
Rob Go <mclordgt@gmail.com>
2023-04-19 07:19:00 +0800

    assigned website token

--- services/ListingsServices.js
+++ services/ListingsServices.js
@@ -1,9 +1,10 @@
-//const baseUrl = 'http://api.laravel.test/api/'
-const baseUrl = 'http://api.laravel.test/api/'
+const baseUrl = 'http://api.housinginteractive.com.ph/'
+// const baseUrl = 'http://api.laravel.test/api/'
 const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: 'Bearer 4|urISlJJjvgtJ8ovkxQLDUPkujiAOuvDCE3teQJ2u'
+    // Authorization: 'Bearer 4|urISlJJjvgtJ8ovkxQLDUPkujiAOuvDCE3teQJ2u' -- Local
+    Authorization: 'Bearer 3|oSlUG6WWIIN28KbNAPSBCmsy9VafmozA1sgbnMfv' //website
 }
 
 export default {
