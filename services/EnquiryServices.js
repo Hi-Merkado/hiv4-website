850d6a7db3eaba0deee9cf8068bd81e9bfe8837c
robeygo <34327876+robeygo@users.noreply.github.com>
2023-08-01 23:18:21 +0800

    Update EnquiryServices.js
    updated online website token

--- services/EnquiryServices.js
+++ services/EnquiryServices.js
@@ -1,10 +1,10 @@
-// const baseUrl = 'http://api.housinginteractive.com.ph/api/'
-const baseUrl = 'http://api.laravel.test/api/'
+const baseUrl = 'http://api.housinginteractive.com.ph/api/'
+//const baseUrl = 'http://api.laravel.test/api/'
 const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: 'Bearer 5|ZeLetv0qjQ4S31UxlObPjbkelaQa9VRBisvf0f4U' // Local
-    // Authorization: 'Bearer 4|xGskwbaKxUi05a4xr8JAIx6ReP1bHQ3OCGVSw0iG' //website
+    //Authorization: 'Bearer 5|ZeLetv0qjQ4S31UxlObPjbkelaQa9VRBisvf0f4U' // Local
+    Authorization: 'Bearer 6|pGpJOLEugVA24Bw2YAlg7mhz2jxCWZipDkxErDkp' //website
 }
 
 export default {
