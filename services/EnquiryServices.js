730091a27e9b1b8983b75e9a3832106292f79b4e
robeygo <34327876+robeygo@users.noreply.github.com>
2023-08-01 23:08:58 +0800

    Update EnquiryServices.js
    updated online website token

--- services/EnquiryServices.js
+++ services/EnquiryServices.js
@@ -4,7 +4,7 @@ const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
     //Authorization: 'Bearer 4|urISlJJjvgtJ8ovkxQLDUPkujiAOuvDCE3teQJ2u' // Local
-    Authorization: 'Bearer 4|xGskwbaKxUi05a4xr8JAIx6ReP1bHQ3OCGVSw0iG' //website
+    Authorization: 'Bearer 6|pGpJOLEugVA24Bw2YAlg7mhz2jxCWZipDkxErDkp' //website
 }
 
 export default {
