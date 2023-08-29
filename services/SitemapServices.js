67b088cdff29f60e3b832df2da028cc5165e508f
Rob Go <mclordgt@gmail.com>
2023-08-29 21:21:37 +0800

    updated token

--- services/SitemapServices.js
+++ services/SitemapServices.js
@@ -1,8 +1,15 @@
-const baseUrl = 'http://api.laravel.test/api/'
+// const baseUrl = 'http://api.laravel.test/api/'
+// const headers = {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//     Authorization: 'Bearer 7|dyKUPz6VsxRUAz6hOOYtKAnbh0tEolcl9fXotr9k'
+// }
+
+const baseUrl = 'https://api.housinginteractive.com.ph/api/'
 const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: 'Bearer 7|dyKUPz6VsxRUAz6hOOYtKAnbh0tEolcl9fXotr9k'
+    Authorization: 'Bearer 8|6ohfzWsOAXFbS6VFMfagZWb1OA1CiJg42ciDKVaX'
 }
 
 export default {
